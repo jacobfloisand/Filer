93503cd963aaeae78a7075155f62fd0c41b6ca6b
Jacob Floisand <jacobfloisand@gmail.com>
2018-06-13 20:48:09 -0600

    Make View

---
+++ FilerClient/FilerView/FilerView.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace FilerClient
+{
+    partial class FilerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.ClassComboBox = new System.Windows.Forms.ComboBox();
+            this.UnitComboBox = new System.Windows.Forms.ComboBox();
+            this.SectionComboBox = new System.Windows.Forms.ComboBox();
+            this.TypeComboBox = new System.Windows.Forms.ComboBox();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ClassLabel = new System.Windows.Forms.Label();
+            this.UnitLabel = new System.Windows.Forms.Label();
+            this.SectionLabel = new System.Windows.Forms.Label();
+            this.TypeLabel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SaveLinkButton = new System.Windows.Forms.Button();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ListButton = new System.Windows.Forms.Button();
+            this.FiledLabel = new System.Windows.Forms.Label();
+            this.UnfiledLabel = new System.Windows.Forms.Label();
+            this.ToDoCheckedList = new System.Windows.Forms.CheckedListBox();
+            this.ResultListBox = new System.Windows.Forms.ListBox();
+            this.UnfiledListBox = new System.Windows.Forms.ListBox();
+            this.FiledListBox = new System.Windows.Forms.ListBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.DateButton = new System.Windows.Forms.Button();
+            this.DateTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(63, 26);
+            this.TitleLabel.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(285, 116);
+            this.TitleLabel.TabIndex = 0;
+            this.TitleLabel.Text = "Filer";
+            // 
+            // ClassComboBox
+            // 
+            this.ClassComboBox.FormattingEnabled = true;
+            this.ClassComboBox.Items.AddRange(new object[] {
+            "Custom..."});
+            this.ClassComboBox.Location = new System.Drawing.Point(21, 216);
+            this.ClassComboBox.Name = "ClassComboBox";
+            this.ClassComboBox.Size = new System.Drawing.Size(121, 21);
+            this.ClassComboBox.TabIndex = 5;
+            // 
+            // UnitComboBox
+            // 
+            this.UnitComboBox.FormattingEnabled = true;
+            this.UnitComboBox.Items.AddRange(new object[] {
+            "Custom..."});
+            this.UnitComboBox.Location = new System.Drawing.Point(157, 216);
+            this.UnitComboBox.Name = "UnitComboBox";
+            this.UnitComboBox.Size = new System.Drawing.Size(121, 21);
+            this.UnitComboBox.TabIndex = 6;
+            // 
+            // SectionComboBox
+            // 
+            this.SectionComboBox.FormattingEnabled = true;
+            this.SectionComboBox.Items.AddRange(new object[] {
+            "Custom..."});
+            this.SectionComboBox.Location = new System.Drawing.Point(293, 216);
+            this.SectionComboBox.Name = "SectionComboBox";
+            this.SectionComboBox.Size = new System.Drawing.Size(121, 21);
+            this.SectionComboBox.TabIndex = 7;
+            // 
+            // TypeComboBox
+            // 
+            this.TypeComboBox.FormattingEnabled = true;
+            this.TypeComboBox.Items.AddRange(new object[] {
+            "Custom..."});
+            this.TypeComboBox.Location = new System.Drawing.Point(293, 158);
+            this.TypeComboBox.Name = "TypeComboBox";
+            this.TypeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.TypeComboBox.TabIndex = 4;
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Location = new System.Drawing.Point(21, 139);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(30, 13);
+            this.DateLabel.TabIndex = 7;
+            this.DateLabel.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "File Name or Link";
+            // 
+            // ClassLabel
+            // 
+            this.ClassLabel.AutoSize = true;
+            this.ClassLabel.Location = new System.Drawing.Point(21, 197);
+            this.ClassLabel.Name = "ClassLabel";
+            this.ClassLabel.Size = new System.Drawing.Size(32, 13);
+            this.ClassLabel.TabIndex = 9;
+            this.ClassLabel.Text = "Class";
+            // 
+            // UnitLabel
+            // 
+            this.UnitLabel.AutoSize = true;
+            this.UnitLabel.Location = new System.Drawing.Point(160, 196);
+            this.UnitLabel.Name = "UnitLabel";
+            this.UnitLabel.Size = new System.Drawing.Size(26, 13);
+            this.UnitLabel.TabIndex = 10;
+            this.UnitLabel.Text = "Unit";
+            // 
+            // SectionLabel
+            // 
+            this.SectionLabel.AutoSize = true;
+            this.SectionLabel.Location = new System.Drawing.Point(290, 197);
+            this.SectionLabel.Name = "SectionLabel";
+            this.SectionLabel.Size = new System.Drawing.Size(43, 13);
+            this.SectionLabel.TabIndex = 11;
+            this.SectionLabel.Text = "Section";
+            // 
+            // TypeLabel
+            // 
+            this.TypeLabel.AutoSize = true;
+            this.TypeLabel.Location = new System.Drawing.Point(290, 139);
+            this.TypeLabel.Name = "TypeLabel";
+            this.TypeLabel.Size = new System.Drawing.Size(31, 13);
+            this.TypeLabel.TabIndex = 12;
+            this.TypeLabel.Text = "Type";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(21, 258);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // SaveLinkButton
+            // 
+            this.SaveLinkButton.Location = new System.Drawing.Point(120, 258);
+            this.SaveLinkButton.Name = "SaveLinkButton";
+            this.SaveLinkButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveLinkButton.TabIndex = 9;
+            this.SaveLinkButton.Text = "Save Link";
+            this.SaveLinkButton.UseVisualStyleBackColor = true;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(230, 258);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(75, 23);
+            this.SearchButton.TabIndex = 10;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // ListButton
+            // 
+            this.ListButton.Location = new System.Drawing.Point(339, 258);
+            this.ListButton.Name = "ListButton";
+            this.ListButton.Size = new System.Drawing.Size(75, 23);
+            this.ListButton.TabIndex = 11;
+            this.ListButton.Text = "List";
+            this.ListButton.UseVisualStyleBackColor = true;
+            // 
+            // FiledLabel
+            // 
+            this.FiledLabel.AutoSize = true;
+            this.FiledLabel.Location = new System.Drawing.Point(24, 505);
+            this.FiledLabel.Name = "FiledLabel";
+            this.FiledLabel.Size = new System.Drawing.Size(29, 13);
+            this.FiledLabel.TabIndex = 19;
+            this.FiledLabel.Text = "Filed";
+            // 
+            // UnfiledLabel
+            // 
+            this.UnfiledLabel.AutoSize = true;
+            this.UnfiledLabel.Location = new System.Drawing.Point(696, 505);
+            this.UnfiledLabel.Name = "UnfiledLabel";
+            this.UnfiledLabel.Size = new System.Drawing.Size(40, 13);
+            this.UnfiledLabel.TabIndex = 20;
+            this.UnfiledLabel.Text = "Unfiled";
+            // 
+            // ToDoCheckedList
+            // 
+            this.ToDoCheckedList.FormattingEnabled = true;
+            this.ToDoCheckedList.Location = new System.Drawing.Point(551, 26);
+            this.ToDoCheckedList.Name = "ToDoCheckedList";
+            this.ToDoCheckedList.Size = new System.Drawing.Size(180, 349);
+            this.ToDoCheckedList.TabIndex = 15;
+            // 
+            // ResultListBox
+            // 
+            this.ResultListBox.FormattingEnabled = true;
+            this.ResultListBox.Location = new System.Drawing.Point(21, 287);
+            this.ResultListBox.Name = "ResultListBox";
+            this.ResultListBox.Size = new System.Drawing.Size(393, 212);
+            this.ResultListBox.TabIndex = 12;
+            // 
+            // UnfiledListBox
+            // 
+            this.UnfiledListBox.FormattingEnabled = true;
+            this.UnfiledListBox.Location = new System.Drawing.Point(21, 522);
+            this.UnfiledListBox.Name = "UnfiledListBox";
+            this.UnfiledListBox.Size = new System.Drawing.Size(338, 95);
+            this.UnfiledListBox.TabIndex = 13;
+            // 
+            // FiledListBox
+            // 
+            this.FiledListBox.FormattingEnabled = true;
+            this.FiledListBox.Location = new System.Drawing.Point(378, 522);
+            this.FiledListBox.Name = "FiledListBox";
+            this.FiledListBox.Size = new System.Drawing.Size(365, 95);
+            this.FiledListBox.TabIndex = 14;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(160, 158);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(118, 20);
+            this.NameTextBox.TabIndex = 3;
+            // 
+            // DateButton
+            // 
+            this.DateButton.Location = new System.Drawing.Point(120, 158);
+            this.DateButton.Name = "DateButton";
+            this.DateButton.Size = new System.Drawing.Size(21, 20);
+            this.DateButton.TabIndex = 2;
+            this.DateButton.Text = "C";
+            this.DateButton.UseVisualStyleBackColor = true;
+            // 
+            // DateTextBox
+            // 
+            this.DateTextBox.Location = new System.Drawing.Point(21, 158);
+            this.DateTextBox.Name = "DateTextBox";
+            this.DateTextBox.Size = new System.Drawing.Size(95, 20);
+            this.DateTextBox.TabIndex = 1;
+            // 
+            // FilerView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 629);
+            this.Controls.Add(this.DateTextBox);
+            this.Controls.Add(this.DateButton);
+            this.Controls.Add(this.NameTextBox);
+            this.Controls.Add(this.FiledListBox);
+            this.Controls.Add(this.UnfiledListBox);
+            this.Controls.Add(this.ResultListBox);
+            this.Controls.Add(this.ToDoCheckedList);
+            this.Controls.Add(this.UnfiledLabel);
+            this.Controls.Add(this.FiledLabel);
+            this.Controls.Add(this.ListButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SaveLinkButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.TypeLabel);
+            this.Controls.Add(this.SectionLabel);
+            this.Controls.Add(this.UnitLabel);
+            this.Controls.Add(this.ClassLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.TypeComboBox);
+            this.Controls.Add(this.SectionComboBox);
+            this.Controls.Add(this.UnitComboBox);
+            this.Controls.Add(this.ClassComboBox);
+            this.Controls.Add(this.TitleLabel);
+            this.Name = "FilerView";
+            this.Text = "FilerView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.ComboBox ClassComboBox;
+        private System.Windows.Forms.ComboBox UnitComboBox;
+        private System.Windows.Forms.ComboBox SectionComboBox;
+        private System.Windows.Forms.ComboBox TypeComboBox;
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label ClassLabel;
+        private System.Windows.Forms.Label UnitLabel;
+        private System.Windows.Forms.Label SectionLabel;
+        private System.Windows.Forms.Label TypeLabel;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button SaveLinkButton;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button ListButton;
+        private System.Windows.Forms.Label FiledLabel;
+        private System.Windows.Forms.Label UnfiledLabel;
+        private System.Windows.Forms.CheckedListBox ToDoCheckedList;
+        private System.Windows.Forms.ListBox ResultListBox;
+        private System.Windows.Forms.ListBox UnfiledListBox;
+        private System.Windows.Forms.ListBox FiledListBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Button DateButton;
+        private System.Windows.Forms.TextBox DateTextBox;
+    }
+}
---
+++ FilerClient/FilerView/FilerView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilerClient
+{
+    public partial class FilerView : Form
+    {
+        public FilerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
